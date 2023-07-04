262f7ede79ac2a20fefca1acc534b7b0e24afc54
Mykhailo21T <eaamysh@students.eaaa.dk>
2023-07-04 15:01:05 +0200

    Summary

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+
+  useEffect(() => {
+    // Accepts the current latitude and longitude
+    // and updates the state variables
+    function setPosition({ coords: { latitude, longitude } }) {
+      setLongitude(longitude);
+      setLatitude(latitude);
+    }
+
+    // Use navigator to get access to the devices' GPS
+    console.log(navigator);
+    navigator.geolocation.getCurrentPosition(setPosition);
+
+    // Update the coordinates as the device is moved
+    let watcher = navigator.geolocation.watchPosition(
+      setPosition,
+      (err) => console.error(err),
+      { enableHighAccuracy: true }
+    );
+
+    // When the component is unmounted, clear the watcher
+    // since it is no longer necessary to track location
+    return () => {
+      navigator.geolocation.clearWatch(watcher);
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.mapView}
+        showsUserLocation
+        followUserLocation
+        showsPointsOfInterest={false}
+      >
+        <Marker
+          title="Seneca College Newnham Campus"
+          description="Educational Institute"
+          coordinate={{ latitude: latitude, longitude: longitude }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +53,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "ghostwhite",
+  },
+
+  mapView: {
+    alignSelf: "stretch",
+    height: "100%",
   },
 });
